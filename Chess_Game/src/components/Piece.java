eb93c2654c287e3772f3d0ac7b7e8b9ccf13a4ad
rm-bias <sandhya.at.18@gmail.com>
2022-12-20 22:59:06 +0530

    completed main functionalities of piece class

--- Chess_Game/src/components/Piece.java
+++ Chess_Game/src/components/Piece.java
@@ -1,10 +1,36 @@
 package components;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Color color;
 	protected Location location;
 	protected String symbol;
+	protected Board board;
+	
+	//constructor
+	public Piece(Color color, Location location, String symbol, Board board) {
+		super();
+		this.color = color;
+		this.location = location;
+		
+		this.location.setPiece(this);
+		this.symbol = symbol;
+		this.board = board;
+	}
+	
+	//getter setters
+	public Location getLocation() {
+		return location;
+	}
+	
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	
+	//abstract methods to move a piece to some location and to check whether a moveis valid
+	public abstract void moveToLocation(Location newLocation) throws InvalidMoveException;
+	
+	protected abstract boolean isValidMove(Location To) throws InvalidMoveException;
 	
 	
 
