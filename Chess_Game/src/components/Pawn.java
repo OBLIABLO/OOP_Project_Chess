f705e7f44a8bdc7660aa6d88949fc2a7ad61596a
rm-bias <sandhya.at.18@gmail.com>
2022-12-21 06:43:16 +0530

    added pawn class

--- Chess_Game/src/components/Pawn.java
+++ Chess_Game/src/components/Pawn.java
@@ -1,5 +1,32 @@
 package components;
 
-public class Pawn {
+public class Pawn extends Piece{
+	
+	public Pawn(Color color, Location location, Board board) {
+		super(color, location, "P", board);
+	}
+
+	@Override
+	public void moveToLocation(Location newLocation) throws InvalidMoveException {
+		if (isValidMove(newLocation)) {
+			//board.movePiece(location, newLocation);
+		} else {
+			throw new InvalidMoveException(InvalidMoveException.PAWN);
+		}
+		
+	}
+
+	@Override
+	protected boolean isValidMove(Location To) throws InvalidMoveException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public String toStringType() {
+		return "Pawn ";
+	}
+
+
 
 }
