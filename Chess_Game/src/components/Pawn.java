7dfb77837d183b70b63910eab44a2d33d61c3833
rm-bias <sandhya.at.18@gmail.com>
2022-12-21 20:46:19 +0530

    completed rook and pawn classes

--- Chess_Game/src/components/Pawn.java
+++ Chess_Game/src/components/Pawn.java
@@ -9,17 +9,79 @@ public class Pawn extends Piece{
 	@Override
 	public void moveToLocation(Location newLocation) throws InvalidMoveException {
 		if (isValidMove(newLocation)) {
-			//board.movePiece(location, newLocation);
+			board.movePiece(location, newLocation);
 		} else {
 			throw new InvalidMoveException(InvalidMoveException.PAWN);
 		}
 		
 	}
 
+	
 	@Override
-	protected Boolean isValidMove(Location To) throws InvalidMoveException {
-		// TODO Auto-generated method stub
+	public Boolean isValidMove(Location locTo) throws InvalidMoveException {
+		if (isMoveForward(locTo)) {
+			if (locTo.getPiece() == null && isValidWhenNotCapturing(locTo)) {
+				return true;
+			} else if (locTo.getPiece() != null && isValidWhenCapturing(locTo)) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			throw new InvalidMoveException(InvalidMoveException.PAWN_ONLY_FORWARD);
+		}
+	}
+	
+	//Checks if the pawn moves on a diagonal or anti diagonal path when it is capturing a piece
+	private Boolean isValidWhenCapturing(Location locTo) throws InvalidMoveException {
+
+		if (((location.getColumn() - locTo.getColumn() == 1) || (location.getColumn() - locTo.getColumn() == -1))
+				&& (location.getRow() - locTo.getRow() == 1)) {
+			return true;
+		} else {
+			throw new InvalidMoveException(InvalidMoveException.PAWN_CAPTURE_ONLY_DIAGONALLY);
+		}
+	}
+	
+	//Checks if the pawn moves forwards when it isn't capturing a piece
+	private Boolean isValidWhenNotCapturing(Location locTo) throws InvalidMoveException {
+		if (location.getColumn() == locTo.getColumn()) {
+			if ((location.getRow() - locTo.getRow() == 1) || location.getRow() - locTo.getRow() == -1) {
+				return true;
+			}
+
+			else if (((location.getRow() - locTo.getRow() == 2) || (location.getRow() - locTo.getRow() == -2))
+					&& isTheFirstMove() && board.freeVerticalPath(location, locTo)) {
+				return true;
+			}
+		}
+
 		return false;
+
+	}
+	
+	//Checks if the pawn moves forwards
+	private Boolean isMoveForward(Location locTo) throws InvalidMoveException {
+		if (this.color.equals(color.BLACK) && locTo.getRow() > location.getRow()) {
+			return true;
+		} else if (this.color.equals(color.WHITE) && locTo.getRow() < location.getRow()) {
+			return true;
+		} else {
+			throw new InvalidMoveException(InvalidMoveException.PAWN_ONLY_FORWARD);
+		}
+	}
+	
+	//Checks if this is the first move of the Pawn, so checks if it can move one or two squares front
+	private Boolean isTheFirstMove() throws InvalidMoveException {
+		if (this.color.equals(color.BLACK) && location.getRow() == 1) {
+			return true;
+		}
+		if (this.color.equals(color.WHITE) && location.getRow() == 6) {
+			return true;
+		} else {
+			throw new InvalidMoveException(InvalidMoveException.PAWN_NOT_FIRSTMOVE);
+
+		}
 	}
 	
 	@Override
--- Chess_Game/src/components/Rook.java
+++ Chess_Game/src/components/Rook.java
@@ -1,7 +1,37 @@
 package components;
 
-public class Rook  {
-	
+public class Rook extends Piece {
+
+	public Rook(Color color, Location location, String symbol, Board board) {
+		super(color, location, symbol, board);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void moveToLocation(Location newLocation) throws InvalidMoveException {
+		if (isValidMove(newLocation)) {
+			board.movePiece(location, newLocation);
+		} else {
+			throw new InvalidMoveException(InvalidMoveException.ROOK);
+		}
+	}
+
+	@Override
+	protected Boolean isValidMove(Location locTo) throws InvalidMoveException {
+
+		if (location.isHorizontalTo(locTo) && board.freeHorizontalPath(location, locTo)) {
+			return true;
+		} else if (location.isVerticalTo(locTo) && board.freeVerticalPath(location, locTo)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String toStringType() {
+		return "Rook ";
+	}
 
 
 
